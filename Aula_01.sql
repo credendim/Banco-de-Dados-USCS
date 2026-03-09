47f9317b3aa34b352628f6571c27c8e25cf56ccc
Gabriel Credendio Furtunato <130668967+credendim@users.noreply.github.com>
2026-03-09 20:55:41 -0300

    Comentarios adicionados

--- Aula_01.sql
+++ Aula_01.sql
@@ -7,16 +7,19 @@ CREATE TABLE Clientes(
     Credit_Limit DECIMAL (10,2)
 );
 
+-- Inserir CLientes a tabela Clientes
 INSERT INTO Clientes(id, Nome, Street, City, State, Credit_Limit)
 VALUES (1, 'Pedro Augusto da Rocha', 'Rua Pedro Carlos Hoffman', 'Porto Alegre', 'RS', 700.00),
 	(2, 'Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', 3500.50),
     (3, 'Luiza Augusta Mhor', 'Rua Salto Grande', 'Niteroi', 'RJ', 4000.00),
     (4, 'Jane Ester', 'Av 7 de setembro', 'Erechim', 'RS', 800.00),
     (5, 'Marcos Antônio dos Santos', 'Av Farrapos', 'Porto Alegre', 'RS', 4250.25);
-    
+
+-- Verifica a inseção de clientes a tabela
 SELECT *
 FROM Clientes;
-    
+
+-- Selecionar somente clientes que moram em RS
 SELECT Nome
 FROM Clientes
 WHERE State = 'RS';
